4a482d979718583d5dfe00699969cc3d5f25d531
Jerico Lim <111488202+vannicc@users.noreply.github.com>
2023-04-17 20:42:20 +0800

    Create Fibonacci.java

---
+++ Assignments/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+        // Prompt user to input desired range of the sequence
+        System.out.print("Enter range: ");
+        int range = scan.nextInt();
+        
+        int firstTerm = 0;
+        int secondTerm = 1;
+
+        System.out.println("Fibonacci sequence up to " + range + " terms:");
+        for (int i = 0; i < range; i++) {
+           System.out.print(firstTerm + " ");
+
+           int nextTerm = firstTerm + secondTerm;
+           firstTerm = secondTerm;
+           secondTerm = nextTerm;
+        }
+
+        scan.close();
+    }
+}
